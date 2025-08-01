5d8b11292e91dfde5f80efa3be6be3d2c0daf456
nealmtroy <51311144+nealmtroy@users.noreply.github.com>
2025-08-01 16:30:00 -0700

    Update nealmtroy.lua

--- nealmtroy.lua
+++ nealmtroy.lua
@@ -850,7 +850,7 @@ local mountains = {
     { name = "GUNUNG KAHUNG",     pos = Vector3.new(1176, 952, -478) },
     { name = "GUNUNG EDEN",       pos = Vector3.new(-806, 1937, -939) },
     { name = "GUNUNG KRAKATAU",   pos = Vector3.new(1276, 844, -140) },
-    { name = "GUNUNG HOREG",   pos = Vector3.new(-1544, 1018, 514) },
+    { name = "GUNUNG HOREG",   pos = Vector3.new(-1556, 1019, 531) },
 }
 
 local currentIndex = 1
